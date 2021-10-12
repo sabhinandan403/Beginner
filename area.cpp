547c81d6c680bacb04039b791e5ddb51e246d0ab
Rimpal2003 <92170006+Rimpal2003@users.noreply.github.com>
2021-10-12 16:22:52 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int area,length,breadth;
+    cout<<"enter the value of length and breadth";
+    cin>>length>>breadth;
+    area=length*breadth;
+    cout<<"area"<<area;
+
+    return 0;
+}
