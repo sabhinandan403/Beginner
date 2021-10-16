73b92f0b080faedb4b0f9238847b66626b1cc82b
meabhi <46453858+AbhimanyuSaini@users.noreply.github.com>
2021-10-16 21:04:57 +0530

    Create table.cpp

---
+++ table.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the no. of which you want to print table"<<endl;
+    cin>>a;
+    for(int i=0;i<=20;i++)
+      cout<<a<<"*"<<i<<"="<<a*i<<endl;
+    
+}
